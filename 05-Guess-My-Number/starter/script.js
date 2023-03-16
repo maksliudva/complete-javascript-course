844e967bc68d53e8c364dea94ffcbfbed6b2ab52
Maks Liudva <73185538+maksliudva@users.noreply.github.com>
2023-03-16 20:51:18 +0000

    changed script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// console.log(document.querySelector(".message").textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 20;
+document.querySelector('.score').textContent = 20;
+
+
+document.querySelector('.guess').value = 100;
+console.log(document.querySelector('.guess').value);
