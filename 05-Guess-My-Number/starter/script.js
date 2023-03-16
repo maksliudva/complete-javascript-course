66f7953722fa79d76129a748ce39559767af488e
Maks Liudva <73185538+maksliudva@users.noreply.github.com>
2023-03-16 21:09:46 +0000

    Plz

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // console.log(document.querySelector(".message").textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -10,3 +10,16 @@ document.querySelector('.score').textContent = 20;
 
 document.querySelector('.guess').value = 100;
 console.log(document.querySelector('.guess').value);
+*/
+
+
+document.querySelector('.check').addEventListener('click', function () {
+
+     const number = document.querySelector('.number').textContent
+     const guess = Number(document.querySelector('.guess').value);
+
+
+     if (!guess) {
+          document.querySelector('.message').textContent = 'No number!';
+     }
+})
